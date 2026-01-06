0a15a4210325f8910d842edce414f234de4a64fe
Lynn El Moussaoui <lynnelmoussaoui@gmail.com>
2026-01-06 12:11:13 +0000

    Fix: remove unused pipeline parameters

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -3,9 +3,4 @@ from mlproject.pipelines.training_pipeline import pipeline
 
 pipeline.upsert(role_arn=os.environ["SAGEMAKER_ROLE_ARN"])
 
-pipeline.start(
-    parameters={
-        "TrainDataS3": "s3://your-bucket/data/train",
-        "TargetColumn": "trip_duration",
-    }
-)
+pipeline.start()
